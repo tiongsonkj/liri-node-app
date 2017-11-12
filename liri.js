a30972aceef37f7bcdde6f7faa379a74d1239c71
Kelvin Tiongson <kelvintiongson30@gmail.com>
2017-11-11 19:52:41 -0600

    finished twitter, starting spotify

--- liri.js
+++ liri.js
@@ -4,12 +4,51 @@ var twitterKeys = require("./keys.js");
 
 var action = process.argv[2];
 
+var twitter = require('twitter');
+
+var client = new twitter({
+  consumer_key: '75WTYslruY1x4V6TutRkkvZyf',
+  consumer_secret: 'GVoRiXSnumsZbrzOqFm5h3h7MW5nSGGRQzrpqTrbsHlYJr02lF',
+  access_token_key: '451296932-ZOjjB8CVliiQk6HpB612VlKQrk0tnpdlBjeeyG74',
+  access_token_secret: '4EcYWptj1eWpFZGzDvsEaB6PvM9VpQ1vTS61FBuyvkVA9'
+});
+
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+  id: '2867a68e1a384f449c6427645b390bed',
+  secret: '1bba95915dce48468fd1500ad0a148be'
+});
+ 
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+});
+
 if (action === "my-tweets") {
-	console.log("MY TWEETS");
-} else if (action === "spotify-this-song") {
-	console.log("YOU'VE BEEN SPOTIFIED");
-} else if (action === "movie-this") {
-	console.log("MOVIE HAS BEEN FOUND");
-} else if (action === "do-what-it-says") {
-	console.log("WHAT AM I DOING?!");
+	var params = {count: '10'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  if (error) {
+	  	console.log(error);
+	  } else {
+	  	for (var key in tweets) {
+		  	console.log("------------------------------");
+		    console.log('"' + tweets[key].text + '"');
+		    console.log("Date tweeted: " + tweets[key].created_at);
+		    console.log("------------------------------");
+		}	
+	  }
+	});
 }
+// if (action === "my-tweets") {
+// 	console.log("MY TWEETS");
+// } else if (action === "spotify-this-song") {
+// 	console.log("YOU'VE BEEN SPOTIFIED");
+// } else if (action === "movie-this") {
+// 	console.log("MOVIE HAS BEEN FOUND");
+// } else if (action === "do-what-it-says") {
+// 	console.log("WHAT AM I DOING?!");
+// }
